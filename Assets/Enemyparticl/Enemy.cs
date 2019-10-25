dc3ac3259585a616846f158781e682bcadefd89d
さず 神原 <hibisyun430@gmail.com>
2019-10-25 12:24:02 +0900

    敵の出現とレーダーのｽｸﾘﾌﾟﾄ

---
+++ Assets/Enemyparticl/Enemy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    MeshRenderer meshrende;
+    void Start()
+    {
+        meshrende = GetComponent<MeshRenderer>();
+        meshrende.material.color = new Color(0, 0, 0, 0.0f);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    //敵が見えるようになる
+    public void Entry()
+    {
+        meshrende.material.color = new Color(0, 0, 0, 1.0f);
+        Debug.Log("敵を発見");
+    }
+}
